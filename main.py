826707fd8c36bcc4c1a0dbc300bb512a07162a3d
Prasham soni <soniprasham@gmail.com>
2022-03-01 14:47:06 +0530

    info

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
+    print("i am Prasham Soni  currently a Instrumentation and Control Student at Nirma University  " )
+    print("This project might help people working with Aruco marker and not able to find a repo that works...:)  ")
+    print("This project Consists Python code that help in Computer Vision and Image Processing ")
+    print("if You are working with Aruco :) you would find this project useful to find the distance between the aruco markers and "
+          "estimate the pose after caibrrating the camera ")
+    print("do follow if you find it useful")
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    name1 = input("Enter Your Name")
+
+    print_hi(name1)
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
